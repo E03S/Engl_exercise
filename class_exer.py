b7869aac0ac73f744af8d9641722f54e52a5300f
E03S <82018652+E03S@users.noreply.github.com>
2023-07-11 00:34:49 +0300

    Add files via upload

---
+++ class_exer.py
@@ -0,0 +1,203 @@
+import spacy 
+import random 
+import pandas as pd
+from nltk.tokenize import sent_tokenize,word_tokenize
+import lemminflect 
+
+class EngExer:
+    
+    def __init__(self):
+        self.__text = ''
+        self.__nlp = spacy.load('en_core_web_sm')
+        self.__dataframe = pd.DataFrame(columns = ['raw','type','task','options','answer'])
+        self.__sent = []
+        self.__position = 0
+        
+        
+    def load_path(self, file_path):
+        with open(file_path) as file:
+            self.__text = file.read()
+        self.__sent = sent_tokenize(self.__text)
+
+    def load_text(self, text):
+        self.__sent = sent_tokenize(text)
+        
+    def verb_replace(self, hard = False):
+        doc = self.__nlp(self.__sent[self.__position])
+        words = []
+        pos = []
+        options = []
+        correct = []
+        raw = []
+        for w in doc:
+            words.append(w.text)
+            pos.append(w.pos_)
+        if not 'VERB' in pos:
+            #print(self.__position)
+            self.__position+=1
+            return None
+        for i in range(len(pos)):
+            if pos[i] == 'VERB':
+                temp = []
+                correct.append(words[i])
+                temp.append(words[i])
+                w = list( lemminflect.getAllInflections(lemminflect.getLemma(words[i], upos='VERB')[0], upos = "VERB").values() ) 
+                for i in range(len(w)):
+                    if w[i][0] not in temp:
+                        temp.append(w[i][0])
+                options.append(temp[:3])
+                raw.append('___')
+            else:
+                raw.append(words[i])
+        if hard == True:
+            options = None
+        dict = {'raw':[raw],'type':'verb selection', 'task':'Выберете правильную форму глагола','options':[options],'answer': [correct]}
+        self.__position+=1
+        return pd.DataFrame(dict) 
+        
+    def sent_dissection(self):
+    #scale = {'A1': [1,6],'A2': [3, 9],}
+        while True:
+            line = self.__sent[self.__position]
+            words = word_tokenize(line)
+            words_rec = []
+            for i in range(len(words)):
+                if i == 0 and not (words[0].isalpha())  :
+                    words_rec = [words[0]+words[1]]
+                    continue
+                if words[i].isalpha():
+                    words_rec.append(words[i])
+                else:
+                    words_rec[-1] = words_rec[-1]+ words[i]
+            random.shuffle(words_rec)
+            if len(words) >1: #add dif
+                dict = {'raw':line,'type':'sentence recombination', 'task':'Постройте предложение','options':[words_rec],'answer': [line.split()]}
+                self.__position+=1
+                return pd.DataFrame(dict)
+
+    def mlt_sentences(self):
+        sent_true = self.__sent[self.__position:(self.__position+5)]
+        sent_rand = self.__sent[self.__position:(self.__position+5)]
+        random.shuffle(sent_rand)
+        #print(sent_rand)
+        #print(sent_true)
+        dict = {'raw': None,'type': 'Multiple sentence recombination', 'task': 'Выстройте предложения в правильном порядке','options': [sent_rand],'answer': [sent_true]}
+        #print(dict)
+        self.__position+=1
+        return pd.DataFrame(dict)
+        
+    def conj_rem(self):
+        doc = self.__nlp(self.__sent[self.__position])
+        words = []
+        pos = []
+        raw = [self.__sent[self.__position-1]]
+        correct = []
+        for w in doc:
+            words.append(w.text)
+            pos.append(w.pos_)
+        if not ('CONJ' in pos) and not ('CCONJ' in pos) and  not ('SCONJ' in pos):
+            #print(self.__position)
+            self.__position+=1
+            return None
+        #correct = self.__sent[self.__position]
+        for i in range(len(words)):
+            if pos[i] in ['CONJ','CCONJ','SCONJ']:
+                raw.append('___')
+                correct.append(words[i])
+            else:
+                raw.append(words[i])
+        #print(self.__position)
+        dict = {'raw':[raw],'type':'Conjunction writing', 'task':'Напишите правильное соединение','options':None,'answer': [correct]}
+        self.__position+=1
+        return pd.DataFrame(dict)
+
+    def det_rem(self):
+        doc = self.__nlp(self.__sent[self.__position])
+        words = []
+        pos = []
+        raw = []
+        correct = []
+        for w in doc:
+            words.append(w.text)
+            pos.append(w.pos_)
+        if not 'DET' in pos:
+            #print(self.__position)
+            self.__position+=1
+            return None
+        #correct = self.__sent[self.__position]
+        for i in range(len(words)):
+            if pos[i] == 'DET':
+                raw.append('___')
+                correct.append(words[i])
+            else:
+                raw.append(words[i])
+        dict = {'raw':[raw],'type':'Det writing', 'task':'Напишите правильный артикль','options':None,'answer': [correct]}
+        #print(self.__position)
+        self.__position+=1
+        return pd.DataFrame(dict)
+
+    def set_params(self, verb_r,sent_d, mlt_sent, det_rem,conj_r):
+        count = verb_r+sent_d+mlt_sent*5+conj_r
+        self.__position = random.randint(0,int( (len(self.__sent)-count)/2 ) )
+        df_final =pd.DataFrame()
+
+        for i in range(verb_r): 
+            while True:
+                w = self.verb_replace()
+                if type(w) == type(pd.DataFrame()):
+                    df_final = pd.concat([df_final,w])
+                    break
+        for i in range(sent_d):
+            df_final = pd.concat([df_final,self.sent_dissection()])
+        for i in range(mlt_sent):
+            df_final = pd.concat([df_final,self.mlt_sentences()])
+        temp = self.__position
+        for i in range(det_rem): 
+            while True:
+                w = self.det_rem()
+                if type(w) == type(pd.DataFrame()):
+                    df_final = pd.concat([df_final,w])
+                    break
+        self.__position = temp
+        temp = self.__position
+        for i in range(conj_r): 
+            while True:
+                w = self.conj_rem()
+                if type(w) == type(pd.DataFrame()):
+                    df_final = pd.concat([df_final,w])
+                    break
+        self.__position = temp
+        
+        return df_final
+        
+''' def noun_syn(self):
+        doc = nlp(self.__sent[self.__position])
+        words = []
+        pos = []
+        options = []
+        correct = []
+            for w in doc:
+            words.append(w.text)
+            pos.append(w.pos_)
+        for i in range(len(pos)):
+            if pos[i] == 'NOUN':
+                temp = []
+                correct.append(words[i])
+                temp.append(words[i])
+                w = list( lemminflect.getAllInflections(lemminflect.getLemma(words[i], upos='VERB')[0], upos = "VERB").values() ) 
+                for i in range(len(w)):
+                    if w[i][0] not in temp:
+                        temp.append(w[i][0])
+                options.append(temp)
+        if hard == True:
+            options = None
+        dict = {'raw':line,'type':'verb selection', 'task':'Выберете правильную форму глагола','options':options,'answer': correct}
+        self.__position+=1
+        return pd.DataFrame(dict) 
+    '''    
+
+    
+
+
+
+        
---
+++ streamlit_app.py
@@ -0,0 +1,112 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import class_exer
+from io import StringIO
+
+
+st.title('Упраженения по английскому языку')
+
+excercise = class_exer.EngExer()
+
+uploaded_file = st.file_uploader('Загрузите текст')
+
+if uploaded_file:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    string_data = stringio.read()
+    excercise.load_text(string_data)
+
+st.header('Введите количество заданий по каждой теме')
+
+verb = st.number_input("Задания на выбор праввильной формы глагола",min_value=0,max_value=15,value =1)
+word = st.number_input("Задания на построение предложений из отдельных слов",min_value=0,max_value=15,value=1)
+mlt_sent = st.number_input("Задания на перестановку предложений в правильном порядке",min_value=0,max_value=5,value=1)
+art = st.number_input("Задания на вписание правильного артикля",min_value=0,max_value=10,value=1)
+con = st.number_input("Задания на вписание правильного соединения",min_value=0,max_value=10,value= 1)
+
+@st.cache_data 
+def gen_text(verb,word,mlt_sent,art,con):
+    return excercise.set_params(verb,word,mlt_sent,art,con)
+
+df = gen_text(verb,word,mlt_sent,art,con)
+    
+position = 0
+if verb!=0:
+    st.header('Выбете праввильную форму глагола')
+    for i in range(verb):
+        result = []
+        st.text(' '.join(df.iloc[[i]]['raw'].values[0]))
+        for j,h in enumerate(df.iloc[[i]]['options'].values[0]):
+            result.append(st.selectbox('nolabel', ['–––'] + h, 
+                                             label_visibility="hidden",key = i*10+j))
+            if result[j] == '–––':
+                pass
+            elif result[j] == df.iloc[[i]]['answer'].values[0][j]:
+                st.success('', icon="✔️")
+            else:
+                st.error('', icon="❌")        
+                
+position += verb         
+if word!=0:
+    st.header('Постройте предложение')
+    for i in range(position,position+word):
+            
+        options = st.multiselect('Варианты',list(df.iloc[[i]]['options'].values[0]),default = None,label_visibility="hidden")
+        #st.write(len(options))
+        #st.write(len(df.iloc[[i]]['answer'].values[0]))
+        if options == None:
+            pass
+        elif len(options) == len(df.iloc[[i]]['answer'].values[0]):
+            if options ==  df.iloc[[i]]['raw'].values[0]:
+                st.success('',icon='✔️')
+            else:
+                st.error('',icon='❌')
+        else:
+            pass        
+
+position+= word
+if mlt_sent!=0:
+    st.header('Выставьте предложения в правильном порядке')
+    for i in range(position,position+mlt_sent):
+        options = st.multiselect('Варианты',list(df.iloc[[i]]['options'].values[0]),default = None,label_visibility="hidden")
+        #st.write(len(' '.join(options)))
+        #st.write(len(df.iloc[[i]]['answer'].values[0]))
+        if options == None:
+            pass
+        elif len(options) == len(df.iloc[[i]]['answer'].values[0]):
+            if options ==  df.iloc[[i]]['raw'].values[0]:
+                st.success('',icon='✔️')
+            else:
+                st.error('',icon='❌')
+        else:
+            pass
+
+position+= mlt_sent
+if art!=0:
+    st.header('Впишите правильный артикль')
+    for i in range(position,position+art):
+        result = []
+        st.text(' '.join(df.iloc[[i]]['raw'].values[0]))
+        for j,h in enumerate(df.iloc[[i]]['answer'].values[0]):
+            result.append(st.text_input('nolabel',label_visibility="hidden",key = i*10+j))
+            if result[j] == None:
+                pass
+            elif result[j] == df.iloc[[i]]['answer'].values[0][j]:
+                st.success('', icon="✔️")
+            else:
+                st.error('', icon="❌") 
+
+position+= art
+if con!=0:
+    st.header('Впишите правильное соединение')
+    for i in range(position,position+con):
+        result = []
+        st.text(' '.join(df.iloc[[i]]['raw'].values[0]))
+        for j,h in enumerate(df.iloc[[i]]['answer'].values[0]):
+            result.append(st.text_input('nolabel',label_visibility="hidden",key = i*10+j))
+            if result[j] == None:
+                pass
+            elif result[j] == df.iloc[[i]]['answer'].values[0][j]:
+                st.success('', icon="✔️")
+            else:
+                st.error('', icon="❌")
